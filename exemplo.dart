3076775d83e5d3975b9ce3426d9dd471b77c52de
Paulo Vanderley <vanderley.1109@gmail.com>
2024-07-30 15:01:16 -0300

    :page_with_curl: Update file content.

--- exemplo.dart
+++ exemplo.dart
@@ -1,4 +1,17 @@
 // Definindo a superclasse Animal.
+class Animal {
+  String nome;
+  int idade;
+
+  Animal(
+    this.nome,
+    this.idade,
+  );
+
+  void emitirSom() {
+    print("O animal faz um som.");
+  }
+}
 
 // Definindo a subclasse Cachorro.
 
